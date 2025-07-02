bbe20db830d90dd09189ad98f614c344b99b4ccf
nazliozemrek <77180888+nazliozemrek@users.noreply.github.com>
2025-07-01 22:52:11 -0500

    Adding a search button

--- src/app/dashboard/GigList.tsx
+++ src/app/dashboard/GigList.tsx
@@ -16,13 +16,13 @@ type Gig = {
 export default function GigList() {
     const [gigs,setGigs ] = useState<Gig[]>([]);
     const [loading,setLoading] = useState(true);
+    const [searchTerm,setSearchTerm] =useState("");
 
     useEffect(() => {
         const fetchGigs = async () => {
         try {
             const res = await fetch('/api/gigs');
             const data = await res.json();
-
             setGigs(data);
         }   catch (error) {
             console.error('Error fetching gigs:',error);
@@ -35,14 +35,37 @@ export default function GigList() {
 
 }, []);
 
-if(!loading && gigs.length  === 0) {
-    return <div className="text-center mt-6 text-gray-500">No gigs found.</div>;
-}
+
+if(loading) return <div className="text-center mt-6 text-gray-500">Loading ...</div>
+if(!loading && gigs.length  === 0) return <div className="text-center mt-6 text-gray-500">No gigs found.</div>;
+
+// filter gigs
+
+const filteredGigs = gigs.filter((gig) =>
+    gig.title?.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    gig.type?.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    gig.location?.toLowerCase().includes(searchTerm.toLowerCase())
+);
 
 return (
-    <div className="grid gap-6 mt-8">
-        {gigs.map((gig)=>(
-            <div key={gig.id} className="bg-white rounded-xl shadow p-5 border">
+    <div className="mt-8">
+        <input
+        type="text"
+        placeholder="Search by title,type,or location..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        className="mb-6 w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:ring-blue-200"
+        />
+
+    {filteredGigs.length === 0 ? (
+        <div className="text-center text-gray-500">No gigs found</div>
+    ) : (
+        <div className="grid gap-6 mt-8">
+            {filteredGigs.map((gig) => (
+             
+             <div key={gig.id} className="bg-white rounded-xl shadow p-5 border">
+
+            
                 <h2 className="text-xl text-black font-semibold">{gig.title}</h2>
                 <p className="text-gray-500 text-sm">{gig.type} • {gig.location}</p>
                 <p className="text-green-600 font-semibold mt-1">{gig.pay}</p>
@@ -56,12 +79,18 @@ return (
                     View Details
                     
                 </a>
-            </div>
-            
-        ))}
-    </div>
+                </div>
+              ))}
+             </div>
+             )}  
+        </div>
     );
 }
+        
+    
+    
+
+        
  
 
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,21 @@
+'use client'
+
+import { useRouter } from "next/navigation";
+
 export default function HomePage() {
+  const router = useRouter();
+
   return (
-    <main className="p-10">
-      <h1 className="text-3xl font-bold">Welcome to GigHub 👋</h1>
-      <p className="text-gray-600">Your side hustle starts here.</p>
+    <main className="min-h-screen flex flex-col items-center justify-center px-4 bg-gray-500">
+      <h1 className="text-4xl font-bold text-gray-800 mb-4">Welcome to GigHub </h1>
+      <p className="text-gray-600">Your side hustle starts here.Login to get started!</p>
+      <button
+        title="Login"
+        onClick={() => router.push('/login')}
+        className="px-6 py-3 bg-black text-white rounded-lg hover:bg-gray-800 transition"
+        >
+          Go to Login
+        </button>
     </main>
   );
 }
