586cbf830e9b528326a953372a4f68abcb16d375
nazliozemrek <77180888+nazliozemrek@users.noreply.github.com>
2025-07-05 11:26:02 -0500

    Fs change

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -6,14 +6,15 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCXKijUwuXXMDRwzHIiGuGydOhoNYs2GmU",
-  authDomain: "gighub-f8b47.firebaseapp.com",
-  projectId: "gighub-f8b47",
-  storageBucket: "gighub-f8b47.appspot.com",
-  messagingSenderId: "29982744660",
-  appId: "1:29982744660:web:f22e691c342be870a84dc1",
-  measurementId: "G-9YSJY0BVVB"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
+
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 
